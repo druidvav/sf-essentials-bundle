1a1cc79003bf598772e24b37d2717bca8b37a4ea
Антон Власов <druidvav@gmail.com>
2017-06-30 16:11:17 +0300

    RequestAwareTrait for gdeposylka

---
+++ src/Druidvav/EssentialsBundle/Service/ContainerService/RequestAwareTrait.php
@@ -0,0 +1,26 @@
+<?php
+namespace Druidvav\EssentialsBundle\Service\ContainerService;
+
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Trait RequestAwareTrait
+ * @deprecated
+ * @package Druidvav\EssentialsBundle\Service\ContainerService
+ */
+trait RequestAwareTrait
+{
+    use ContainerAwareTrait;
+
+    /**
+     * Shortcut to return the request service.
+     *
+     * @deprecated 
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->container->get('request_stack')->getCurrentRequest();
+    }
+}

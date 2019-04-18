51a12373d64fbb37f8106ab83c5b4009e037b82d
Антон Власов <druidvav@gmail.com>
2019-04-18 19:34:11 +0300

    PhpStorm compatibility

--- src/Druidvav/EssentialsBundle/Command.php
+++ src/Druidvav/EssentialsBundle/Command.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @noinspection PhpInternalEntityUsedInspection
+ * We'll assume that is internal library :)
+ */
 namespace Druidvav\EssentialsBundle;
 
 use Doctrine\ORM\EntityManager;
@@ -6,6 +10,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand as BaseCommand;
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerTrait;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,27 +18,12 @@ use Symfony\Component\Process\Process;
 
 abstract class Command extends BaseCommand
 {
+    /**
+     * In ControllerTrait there are lots of methods incompatible with Command, but we
+     * use it for PhpStorm autocomplete compatibility
+     */
+    use ControllerTrait;
     use LoggerAwareTrait;
-
-    /**
-     * @param string $id The service id
-     * @return bool true if the service id is defined, false otherwise
-     * @final
-     */
-    protected function has($id)
-    {
-        return $this->getContainer()->has($id);
-    }
-
-    /**
-     * @param string $id The service id
-     * @return object The service
-     * @final
-     */
-    protected function get($id)
-    {
-        return $this->getContainer()->get($id);
-    }
 
     /**
      * @param string $name The parameter name
